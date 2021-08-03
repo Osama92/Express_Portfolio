6ab6a31e52fc7a8ffe45250cd63d66e4b30060ea
Osama92 <try2rym@yahoo.com>
2021-08-03 06:54:26 +0100

    Added project view

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const port = 3000
 app.get('/', (req, res)=> {
     res.render('about')
 })
+app.get('/project', (req, res)=> {
+    res.render('project')
+})
 
 app.listen(port, ()=> {
     console.log('App runinng at port ' + `${port}`)
