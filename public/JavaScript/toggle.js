773de3c0cc596b8dde915005c829c9b344a7d204
Osama92 <try2rym@yahoo.com>
2021-08-08 21:43:24 +0100

    Minor addition to toggle js

--- public/JavaScript/toggle.js
+++ public/JavaScript/toggle.js
@@ -2,6 +2,8 @@ const light = 'fas fa-sun'
 const dark = 'fas fa-moon'
 const toggleBtn = document.querySelector("#toggle");
 const headerColor = document.querySelector('.header');
+const introColor = document.querySelector('.about');
+const footerColor = document.querySelector('.footer');
 const text = document.querySelectorAll('a');
 toggleBtn.className = light
 const toggler = document.getElementById("toggler").addEventListener('click', ()=> {
@@ -11,6 +13,8 @@ const toggler = document.getElementById("toggler").addEventListener('click', ()=
         toggleBtn.className = dark
         headerColor.style.transition = 'background 1s ease'
         headerColor.style.background = '#2D2424'
+        
+
         for (let i = 0; i < text.length; i++) {
             text[i].style.color = 'white';
             
@@ -24,4 +28,5 @@ const toggler = document.getElementById("toggler").addEventListener('click', ()=
             
         }
     }
+
 }) 
