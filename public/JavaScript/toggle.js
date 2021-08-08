935dd9a2e15176af4286250edf6a14eccc6813fb
Osama92 <try2rym@yahoo.com>
2021-08-08 21:06:42 +0100

    Added functionality to dark and light toggle btn

--- public/JavaScript/toggle.js
+++ public/JavaScript/toggle.js
@@ -1,13 +1,20 @@
 const light = 'fas fa-sun'
 const dark = 'fas fa-moon'
 const toggleBtn = document.querySelector("#toggle")
+const headerColor = document.querySelector('.header')
+const text = document.querySelector('a')
 toggleBtn.className = light
 const toggler = document.getElementById("toggler").addEventListener('click', ()=> {
     
     if
     (toggleBtn.className === light) {
         toggleBtn.className = dark
+        headerColor.style.background = '#2D2424'
+        text.style.color = 'white'
+
     } else {
         toggleBtn.className = light
+        headerColor.style.background = '#FEF7DC'
+        text.style.color = 'black'
     }
 }) 
