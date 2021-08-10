ff1bf08f3d72a7251980dc183d6d5c5ed60bccd2
Osama92 <try2rym@yahoo.com>
2021-08-10 23:36:25 +0100

    Worked on toggle.js

--- public/JavaScript/toggle.js
+++ public/JavaScript/toggle.js
@@ -1,13 +1,14 @@
 const light = 'fas fa-sun'
 const dark = 'fas fa-moon'
 const toggleBtn = document.querySelector("#toggle");
-const headerColor = document.querySelector('.header');
+const headerColor = document.querySelector('header');
 const introColor = document.querySelector('.about');
 const footerColor = document.querySelector('.footer');
-const text = document.querySelectorAll('a');
+const text = document.querySelectorAll('p');
 toggleBtn.className = light
 
 const colorChange = ()=> {
+    headerColor.classList.add('headerColors')
     introColor.classList.add('introColor')
     footerColor.classList.add('footerColor')
     
@@ -15,30 +16,32 @@ const colorChange = ()=> {
 const colorRemove = ()=> {
     introColor.classList.remove('introColor')
     footerColor.classList.remove('footerColor')
+    headerColor.classList.remove('headerColors')
 }
 const toggler = document.getElementById("toggler").addEventListener('click', ()=> {
     
     if
     (toggleBtn.className === light) {
         toggleBtn.className = dark
-        headerColor.style.transition = 'background 1s ease'
-        headerColor.style.background = '#2D2424'
-        colorChange()
+        // headerColor.style.transition = 'background 1s ease'
+        // headerColor.style.background = '#2D2424'
         
 
         for (let i = 0; i < text.length; i++) {
             text[i].style.color = 'white';
             
         }
+        colorChange()
 
     } else {
         toggleBtn.className = light
-        headerColor.style.background = '#FEF7DC'
-        colorRemove()
+        // headerColor.style.background = '#FEF7DC'
+        
         for (let i = 0; i < text.length; i++) {
             text[i].style.color = 'black';
             
         }
+        colorRemove()
     }
 
 }) 
