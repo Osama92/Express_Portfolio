162954618793eb0b45747248a1240ec488121d19
Osama92 <try2rym@yahoo.com>
2021-08-21 11:17:05 +0100

    Worked on Animation js

--- public/JavaScript/animation.js
+++ public/JavaScript/animation.js
@@ -3,4 +3,6 @@ ScrollReveal().reveal(".emphacy", { delay: 700, duration: 600, });
 ScrollReveal().reveal(".large", { delay: 500 });
 ScrollReveal().reveal(".emphacy2", { delay: 1000, distance: '90px', interval: 1000 });
 ScrollReveal().reveal(".catch", { delay: 1000, duration: 2000 });
+ScrollReveal().reveal(".profileImg", {delay:200, duration: 1000, origin:'left', distance: '100px', reset: true})
+ScrollReveal().reveal(".profileImg2", {delay:200, duration: 1000, origin:'right', distance: '100px', reset: true})
 
