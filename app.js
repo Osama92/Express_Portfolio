ba9655cad98b7d4ea6644ba477c54414484a5d50
Osama92 <try2rym@yahoo.com>
2021-08-21 21:09:13 +0100

    Modified App.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended:true}))
 
 
