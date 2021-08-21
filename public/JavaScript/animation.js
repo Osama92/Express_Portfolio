bba9c4f318817c27036e07885719c5ef8f6b77e0
Osama92 <try2rym@yahoo.com>
2021-08-21 12:37:01 +0100

    worked on mobile layout

--- public/JavaScript/animation.js
+++ public/JavaScript/animation.js
@@ -1,7 +1,7 @@
 ScrollReveal().reveal(".hero", { delay: 500 });
 ScrollReveal().reveal(".emphacy", { delay: 700, duration: 600, });
 ScrollReveal().reveal(".large", { delay: 500 });
-ScrollReveal().reveal(".emphacy2, .fab", { delay: 1000, interval: 200, origin: 'bottom',distance: '30px' });
+ScrollReveal().reveal(".emphacy2, .fab", { delay: 1000, interval: 200, origin: 'bottom',distance: '30px', mobile: false });
 ScrollReveal().reveal(".catch", { delay: 1000, duration: 2000 });
 ScrollReveal().reveal(".profileImg", {delay:200, duration: 1000, origin:'left', distance: '100px', reset: true})
 ScrollReveal().reveal(".profileImg2", {delay:200, duration: 1000, origin:'right', distance: '100px', reset: true})
