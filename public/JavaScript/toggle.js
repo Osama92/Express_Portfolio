27e645063d67940bf979c7170221c39c32648188
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-08-27 18:25:22 +0100

    minor update on mobile

--- public/JavaScript/toggle.js
+++ public/JavaScript/toggle.js
@@ -62,17 +62,20 @@ darkBtn.addEventListener("click", (e) => {
 
 // Dark mode toggle function for mobile / small screen:
 darkBtn2.addEventListener("click", (e) => {
+  toggle = localStorage.setItem("darkmode", "Enabled");
   switch (e.target.className) {
     case "bx bx-sun":
       darkBtn2.className = "bx bx-moon";
-      document.documentElement.style.setProperty("--background1", "#343A40");
-      document.documentElement.style.setProperty("--fontColor1", "#fff");
+      // document.documentElement.style.setProperty("--background1", "#343A40");
+      // document.documentElement.style.setProperty("--fontColor1", "#fff");
+      // EnableDarkMode();
 
       break;
     case "bx bx-moon":
       darkBtn2.className = "bx bx-sun";
-      document.documentElement.style.setProperty("--background1", "#fff");
-      document.documentElement.style.setProperty("--fontColor1", "#000");
+      // document.documentElement.style.setProperty("--background1", "#fff");
+      // document.documentElement.style.setProperty("--fontColor1", "#000");
+      // DisableDarkMode();
 
       break;
     default:
