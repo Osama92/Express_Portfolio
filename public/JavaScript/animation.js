8fde08b146f96b5e21802b139478d1956a9b81c9
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-06 19:12:16 +0100

    Minor Changes on project

--- public/JavaScript/animation.js
+++ public/JavaScript/animation.js
@@ -1,7 +1,31 @@
 ScrollReveal().reveal(".hero", { delay: 500 });
-ScrollReveal().reveal(".emphacy", { delay: 700, duration: 600, });
+ScrollReveal().reveal(".emphacy", { delay: 900, duration: 600 });
+ScrollReveal().reveal(".showcase", {
+  delay: 700,
+  duration: 600,
+  origin: "left",
+  distance: "30px"
+});
 ScrollReveal().reveal(".large", { delay: 500 });
-ScrollReveal().reveal(".emphacy2, .fab", { delay: 1000, interval: 200, origin: 'bottom',distance: '30px', mobile: false });
+ScrollReveal().reveal(".emphacy2, .fab, #proj_icons", {
+  delay: 1000,
+  interval: 200,
+  origin: "bottom",
+  distance: "30px",
+  mobile: false
+});
 ScrollReveal().reveal(".catch", { delay: 1000, duration: 2000 });
-ScrollReveal().reveal(".profileImg, .mobilepic", {delay:200, duration: 1000, origin:'left', distance: '200px', reset: true})
-ScrollReveal().reveal(".profileImg2", {delay:200, duration: 1000, origin:'right', distance: '100px', reset: true})
+ScrollReveal().reveal(".profileImg, .mobilepic", {
+  delay: 200,
+  duration: 1000,
+  origin: "left",
+  distance: "200px",
+  reset: true
+});
+ScrollReveal().reveal(".profileImg2", {
+  delay: 200,
+  duration: 1000,
+  origin: "right",
+  distance: "100px",
+  reset: true
+});
