7c1e3723e2e72e409173a0c9994874fd262dffd4
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-24 13:05:07 +0100

    working on Firebase

--- public/JavaScript/_firebase.js
+++ public/JavaScript/_firebase.js
@@ -1,25 +1,37 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 
-const firebaseConfig = initializeApp({
+// const firebaseConfig = initializeApp({
+//   apiKey: "AIzaSyBkfJ968njQwfQ9ezSSHNMKOhgSX8emUeQ",
+//   authDomain: "portfolio-973ed.firebaseapp.com",
+//   projectId: "portfolio-973ed",
+//   storageBucket: "portfolio-973ed.appspot.com",
+//   messagingSenderId: "526933837059",
+//   appId: "1:526933837059:web:3845240d7ce7f4f93f9536",
+//   measurementId: "G-PXD1C7DH9F"
+// });
+
+// const db = getDatabase(firebaseConfig);
+
+var config = {
   apiKey: "AIzaSyBkfJ968njQwfQ9ezSSHNMKOhgSX8emUeQ",
   authDomain: "portfolio-973ed.firebaseapp.com",
-  projectId: "portfolio-973ed",
-  storageBucket: "portfolio-973ed.appspot.com",
-  messagingSenderId: "526933837059",
-  appId: "1:526933837059:web:3845240d7ce7f4f93f9536",
-  measurementId: "G-PXD1C7DH9F"
-});
+  databaseURL: "https://portfolio-973ed-default-rtdb.firebaseio.com/",
+  storageBucket: "portfolio-973ed.appspot.com"
+};
+initializeApp(config);
 
-const db = getDatabase(firebaseConfig);
+// Get a reference to the database service
+const db = getDatabase();
 
-function writeUserData(userId, name, email, imageUrl) {
-  set(ref(db, "" + userId), {
-    username: name,
-    email: email,
-    profile_picture: imageUrl
-  });
-}
+// function writeUserData(userId, name, email, imageUrl) {
+//   set(ref(db, "users/" + userId), {
+//     username: name,
+//     email: email,
+//     profile_picture: imageUrl
+//   });
+// }
 
-writeUserData("Dayo", "Ade", "R@gmail.com", "none");
-///console.log(writeUserData);
+// writeUserData("Dayo", "Dayo", "dayo@gmail.com", "hf.png");
+
+console.log(getDatabase);
